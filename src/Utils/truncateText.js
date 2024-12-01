d9e02251b7fc3df33d34b8595a02c2ee4b87a168
pllobkhalil191 <pollob@triumphdigital.co.th>
2024-12-01 17:25:00 +0600

    first commit

--- src/Utils/truncateText.js
+++
@@ -1,7 +0,0 @@
-export const truncateText = (text, maxLength) => {
-    if (text.length <= maxLength) {
-      return text;
-    }
-    return text.substring(0, maxLength) + '...';
-  };
-  
---
+++ src/app/page.js
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import Homepage from "./components/Homepage/Homepage";
+
+export default function Home() {
+  return (
+    <div className="">
+   <Homepage/>
+    </div>
+  );
+}
